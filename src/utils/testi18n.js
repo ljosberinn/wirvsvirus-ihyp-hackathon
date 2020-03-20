02e015862636b284277fca2915569e2a986de719
ljosberinn <admin@gerritalex.de>
2020-03-20 20:33:12 +0100

    adapt i18n to single language use

--- src/utils/testi18n.js
+++ src/utils/testi18n.js
@@ -1,26 +1,18 @@
 import i18n from 'i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import Backend from 'i18next-xhr-backend';
 import { initReactI18next } from 'react-i18next';
 
-import { ENABLED_LANGUAGES } from '../constants/env';
+import resources from '../i18n/de';
 
-i18n
-  .use(Backend)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    fallbackLng: 'de',
-    debug: false,
-    load: 'languageOnly',
-    resources: ENABLED_LANGUAGES.reduce((carry, slug) => {
-      carry[slug] = {};
-      return carry;
-    }, {}),
-    interpolation: {
-      escapeValue: false,
-    },
-    react: { useSuspense: false },
-  });
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'de',
+  fallbackLng: 'de',
+  debug: false,
+  load: 'languageOnly',
+  interpolation: {
+    escapeValue: false,
+  },
+  react: { useSuspense: false },
+});
 
 export default i18n;
