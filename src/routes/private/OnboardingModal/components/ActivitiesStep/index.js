1025227ff8aaee749949f447221897a7de661c2f
ljosberinn <admin@gerritalex.de>
2020-03-21 15:14:50 +0100

    add react-use & use-google-maps-sdk

--- src/routes/private/OnboardingModal/components/ActivitiesStep/index.js
+++ src/routes/private/OnboardingModal/components/ActivitiesStep/index.js
@@ -2,10 +2,15 @@ import PropTypes from 'prop-types';
 import { Title, Field, Control, Input, Label } from 'rbx';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { FaSearch } from 'react-icons/fa';
+import { useGeolocation } from 'react-use';
+import { usePlacePredictions } from 'use-google-maps-sdk';
 
-import { Slider, Checkbox } from '../../../../../components';
+import { Slider, Checkbox, Icon } from '../../../../../components';
 
 const possibleActivities = ['SHOP', 'DOG', 'HOMEWORK', 'SHARE'];
+
+const gmapsKey = process.env.REACT_APP_GOOGLE_MAPS_KEY;
 
 export default function ActivitiesStep({
   handleChange,
@@ -14,6 +19,16 @@ export default function ActivitiesStep({
   activities,
 }) {
   const { t } = useTranslation('onboarding');
+
+  const geolocation = useGeolocation();
+
+  const { results } = usePlacePredictions({
+    key: gmapsKey,
+    country: ['DE', 'AT', 'CH'],
+    radius: radius * 1000,
+    geolocation,
+    input: address,
+  });
 
   return (
     <>
@@ -37,15 +52,22 @@ export default function ActivitiesStep({
       <Field>
         <Label htmlFor="address">{t('address-label')}</Label>
 
-        <Control>
+        <Control iconLeft>
+          <Icon svg={FaSearch} />
           <Input
             id="address"
+            list="address-datalist"
             defaultValue={address}
             name="address"
             onChange={handleChange}
             required
             placeholder={t('address')}
           />
+          <datalist id="address-datalist">
+            {results.map(result => (
+              <option key={result.id}>{result.description}</option>
+            ))}
+          </datalist>
         </Control>
       </Field>
 
