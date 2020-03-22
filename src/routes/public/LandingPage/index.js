e4f5f49e4b90f8bcc0cb26da14422cd0aa5b6566
ljosberinn <admin@gerritalex.de>
2020-03-22 02:18:07 +0100

    setup LandingPage

--- src/routes/public/LandingPage/index.js
+++ src/routes/public/LandingPage/index.js
@@ -1,47 +1,54 @@
-import { Title, Section, Hero, Container, Column, Image } from 'rbx';
+import { Button } from 'rbx';
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-import { TemplatedHelmet } from '../../../components';
-import { LandingPageSvg } from '../../../components/themedSvgs';
+import { TemplatedHelmet, Map } from '../../../components';
+import { useNavigationContext } from '../../../context';
 import { withSentry } from '../../../hocs';
-import './LandingPage.scss';
+import styles from './LandingPage.module.scss';
+
+const defaultLocation = {
+  lng: 48.1828776,
+  lat: 11.5940998,
+};
 
 export default withSentry(function LandingPage() {
+  const {
+    routes: { REGISTER },
+  } = useNavigationContext();
+
   return (
     <>
       <TemplatedHelmet>
         <title>Home</title>
       </TemplatedHelmet>
-      <Hero as={Section} relative className="is-medium is-block">
-        <Container>
-          <Column.Group vcentered className="is-desktop">
-            <Column desktop={{ size: 5 }} className="has-text-centered-touch">
-              <header>
-                <Title
-                  textWeight="bold"
-                  className="is-1 is-size-2-mobile"
-                  spaced
-                  textColor="primary"
-                >
-                  A very catchy slogan
-                </Title>
-                <Title as="p" subtitle>
-                  <span>
-                    Yeah yeah, we get it. You do this, that and probably
-                    something else too.
-                  </span>
-                </Title>
-              </header>
-            </Column>
-            <Column size={1} />
-            <Column>
-              <Image.Container>
-                <LandingPageSvg />
-              </Image.Container>
-            </Column>
-          </Column.Group>
-        </Container>
-      </Hero>
+      <div className={styles.wrap}>
+        <div className={styles.content}>
+          <div className={styles.buttonContainer}>
+            <Button.Group>
+              <Button
+                as={NavLink}
+                to={REGISTER.clientPath}
+                type="button"
+                size="large"
+                color="primary"
+              >
+                Ich will helfen
+              </Button>
+
+              <Button type="button" size="large" color="info">
+                Ich biete Hilfe an
+              </Button>
+            </Button.Group>
+          </div>
+        </div>
+        <Map
+          accessToken="pk.eyJ1IjoicmV0aW5hZGVzaWduIiwiYSI6ImNrODFnazBkMzA4aGUzZ293azg4MGxud3oifQ.jMmSszx6gA2v9OnztjCA5Q"
+          mapStyle="mapbox://styles/retinadesign/ck82bajtp0f6j1il7sek7byl8"
+          defaultLocation={defaultLocation}
+          forceFallback
+        />
+      </div>
     </>
   );
 });
