2f8f944b6c7a7d9f03362b1788a37878342ba54b
ljosberinn <admin@gerritalex.de>
2020-03-22 14:04:47 +0100

    remove unused stuff

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,8 +1,6 @@
 import { Column } from 'rbx';
 import React, { memo } from 'react';
 
-import DrawerNav from '../DrawerNav';
-import Footer from '../Footer';
 import Navbar from '../Navbar';
 import styles from './Layout.module.scss';
 /**
@@ -22,12 +20,10 @@ export default memo(function Layout({ children }) {
         className={styles.main}
         data-testid="layout-main"
       >
-        <DrawerNav data-testid="drawer-nav" />
         <Column className={styles.shadow} data-testid="layout-main-children">
           {children}
         </Column>
       </Column.Group>
-
     </>
   );
 });
