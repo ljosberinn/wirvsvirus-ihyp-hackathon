77c92bc8a8e691b9460b369b4defced053901c3d
ljosberinn <admin@gerritalex.de>
2020-03-22 16:21:53 +0100

    fix sentry lazyloading

--- src/utils/sentry.js
+++ src/utils/sentry.js
@@ -2,6 +2,8 @@ import { init, captureException } from '@sentry/browser';
 
 import { SENTRY_DSN } from '../constants/env';
 
+export { configureScope } from '@sentry/browser';
+
 init({
   dsn: SENTRY_DSN,
   integrations: integrations =>
